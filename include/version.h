11ad3f71e5be6fd03d815dcf641b485651ec2816
viktak <github@viktak.com>
2021-01-28 13:08:48 +0200

    version bump

--- include/version.h
+++ include/version.h
@@ -7,11 +7,11 @@
 ///////////////////////////////////////////////////////////////
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "51"
+  #define BUILD_NUMBER "53"
 #endif
 #ifndef FIRMWARE_VERSION
-  #define FIRMWARE_VERSION "v1.0.51 - 2021-01-28 12:49:56.014291"
+  #define FIRMWARE_VERSION "v1.0.53 - 2021-01-28 13:08:30.739198"
 #endif
 #ifndef FIRMWARE_VERSION_SHORT
-  #define FIRMWARE_VERSION_SHORT "v1.0.51"
+  #define FIRMWARE_VERSION_SHORT "v1.0.53"
 #endif
