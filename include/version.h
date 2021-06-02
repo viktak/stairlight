084daac9095a8263b10c9747c0fcf6418386d250
viktak <github@viktak.com>
2021-06-02 18:35:57 +0300

    exclude version number in git

--- include/version.h
+++ include/version.h
@@ -7,11 +7,11 @@
 ///////////////////////////////////////////////////////////////
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "284"
+  #define BUILD_NUMBER "299"
 #endif
 #ifndef FIRMWARE_VERSION
-  #define FIRMWARE_VERSION "v1.0.284 - 2021-03-24 18:17:30.983112"
+  #define FIRMWARE_VERSION "v1.0.299 - 2021-06-02 18:33:04.787860"
 #endif
 #ifndef FIRMWARE_VERSION_SHORT
-  #define FIRMWARE_VERSION_SHORT "v1.0.284"
+  #define FIRMWARE_VERSION_SHORT "v1.0.299"
 #endif
